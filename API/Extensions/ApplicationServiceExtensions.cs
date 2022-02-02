9ac878133c541993b35d1da1a4087e4249acea1c
cwhays16 <cwhays16@gmail.com>
2022-02-02 11:23:23 -0500

    End of Section 15 - Added Messaging

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,7 @@ namespace API.Extensions
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles));
             services.AddDbContext<DataContext>(options =>
             {
